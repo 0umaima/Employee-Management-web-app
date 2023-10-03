e5ffc874556f1ce581f512dc46e21f4c960178f5
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-10-03 23:22:08 +0100

    update routes for get method

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const addEmployees = require ('../controllers/addEmployees.js');
-const deleteEmployees = require ('../controllers/deleteEmployees.js')
+const deleteEmployees = require ('../controllers/deleteEmployees.js');
+const getEmployees = require('../controllers/getEmployees.js');
 
-
+router.get('/employees', getEmployees.getAllEmployees);
 
 router.post('/employees', addEmployees.addEmployee);
 
