28eb8d59abe070c0a5c35ded4adba35e90ba58e1
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-10-03 22:53:06 +0100

    Add deleteEmployees function in controllers - Update Routes

---
+++ controllers/deleteEmployees.js
@@ -0,0 +1,34 @@
+const connectDb = require('../config/db.js')
+const { Employee } = require('../models/models.js')
+
+    connectDb();
+
+    async function deleteEmployee (req, res){
+        try{
+            //Extract employee id from request params
+            const { id } = req.params;
+
+            //find employee by id and delete it
+            const deletedEmployee = await Employee.findByIdAndDelete(id);
+
+            if(!deletedEmployee){
+                return res.status(404).json({ message: "Employee not Deleted"});
+            }
+
+            res.status(200).json({message: "Employee Deleted Succesfully"});
+
+
+        } catch(err){
+            console.log("Error Deleting Employee", err);
+            res.status(500).json({message: "Server Issue"})
+        }
+};
+
+
+
+
+module.exports= {deleteEmployee};
+
+
+
+
--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const addEmployees = require ('../controllers/addEmployees.js');
+const deleteEmployees = require ('../controllers/deleteEmployees.js')
 
 
 
 router.post('/employees', addEmployees.addEmployee);
+
+router.delete('/employees/:id', deleteEmployees.deleteEmployee)
 
 module.exports = router;
