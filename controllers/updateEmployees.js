e8160505f2510d6c1197746ed00ab6193acdeab7
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-10-04 00:37:10 +0100

    Create Update function to update employee data

---
+++ controllers/updateEmployees.js
@@ -0,0 +1,35 @@
+const connectDb = require ('../config/db.js');
+const { Employee } = require ('../models/models.js');
+
+connectDb();
+
+async function updateEmployee (req, res) {
+    try{
+
+        const {id} = req.params;
+
+        const {name, surname, departement} = req.body;
+
+        const updateEmployeeData = await Employee.findByIdAndUpdate(
+            id, 
+            {name, surname, departement}, 
+            {new: true}
+        );
+
+        
+
+
+        if(!updateEmployeeData){
+            res.status(404).json({message: 'Employee not updated'});
+        }
+        res.status(200).json({message:"Employee Updated Successfully", Employee: updateEmployeeData});
+
+    } catch (error){
+
+        console.log("Error Updating Employee", error);
+        res.status(500).json({message: 'Server Issues'});
+
+    }
+}
+
+module.exports = {updateEmployee};
