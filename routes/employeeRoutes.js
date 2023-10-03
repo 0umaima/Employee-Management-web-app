3b0bda5e4bd5c79155c1635e48c5e160459322db
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-10-04 00:39:04 +0100

    add route for put method

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const addEmployees = require ('../controllers/addEmployees.js');
 const deleteEmployees = require ('../controllers/deleteEmployees.js');
 const getEmployees = require('../controllers/getEmployees.js');
+const updateEmployees = require('../controllers/updateEmployees.js')
 
 router.get('/employees', getEmployees.getAllEmployees);
 
 router.post('/employees', addEmployees.addEmployee);
 
-router.delete('/employees/:id', deleteEmployees.deleteEmployee)
+router.delete('/employees/:id', deleteEmployees.deleteEmployee);
+
+router.put('/employees/:id', updateEmployees.updateEmployee);
 
 module.exports = router;
